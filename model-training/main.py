f5cca05f6ba1573a0a0b375e4be3efe6b2de8092
Numan Kafadar <numankafadar97@gmail.com>
2023-11-13 01:08:03 +0300

    .

--- model-training/main.py
+++ model-training/main.py
@@ -3,5 +3,5 @@ from crop import *
 if __name__ == "__main__":
     path="C:/Users/smurf/Desktop/autsl/train/signer0_sample18_color.mp4" 
     original_frames, cropped_frames, results_list =crop_image(path, 380)
-    selected_frame = 10
+    selected_frame = 20
     show_original_and_cropped_image(original_frames[selected_frame], cropped_frames[selected_frame], results_list[selected_frame])

d9ebf7f9966c64905b1e049206a402ad24d998ae
Numan Kafadar <numankafadar97@gmail.com>
2024-01-14 19:32:02 +0300

    crop fixed

--- model-training/main.py
+++ model-training/main.py
@@ -2,6 +2,8 @@ from crop import *
 
 if __name__ == "__main__":
     path="C:/Users/smurf/Desktop/autsl/train/signer0_sample20_color.mp4" 
-    original_frames, cropped_frames, results_list =crop_image(path, 380)
-    selected_frame = 20
-    show_original_and_cropped_image(original_frames[selected_frame], cropped_frames[selected_frame], results_list[selected_frame])
+    original_frames, cropped_frames ,detection_results=crop_video(path, 380)
+
+    show_original_and_cropped_image(original_frames[0], cropped_frames[0], detection_results[0])
+    show_original_and_cropped_image(original_frames[10], cropped_frames[10], detection_results[10])
+    show_original_and_cropped_image(original_frames[20], cropped_frames[20], detection_results[20])

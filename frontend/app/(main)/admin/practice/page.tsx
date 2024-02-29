dff20036e44d77af4309156f1b0a99bb5221b24e
Numan Kafadar <numankafadar97@gmail.com>
2024-02-29 19:57:20 +0300

    bug fix on frontend. removed primeflex.css

--- frontend/app/(main)/admin/practice/page.tsx
+++ frontend/app/(main)/admin/practice/page.tsx
@@ -11,6 +11,7 @@ const PracticeAdminPage = () => {
   return (
     <div className="card">
       <div className="flex items-center justify-center">
+<<<<<<< Updated upstream
         <Webcam
           className="rounded-lg w-[30rem]"
           audio={false}
@@ -19,6 +20,28 @@ const PracticeAdminPage = () => {
           width={"auto"}
           videoConstraints={videoConstraints}
         ></Webcam>
+=======
+        <div>
+          <div>
+            Kelimelerinizi işaret diliyle kameraya ifade etmek için hazır
+            mısınız? Hemen başlamak için lütfen bir pratik seçin ve kendinizi
+            ifade edin! 🤟✨
+          </div>
+          <Link href={"/admin/practice/standart"}>
+            <div className="bg-[var(--primary-color)] cursor-pointer text-white rounded-lg m-5 p-5 flex items-center justify-between transition duration-300 hover:scale-105">
+              <div>
+                <div className=" font-bold text-lg">Standart Pratik</div>
+                <div className="">
+                  Rastgele seçilmiş kelimeleri ifade etmeye çalışın!
+                </div>
+              </div>
+              <div>
+                <FaRegArrowAltCircleRight size={35}></FaRegArrowAltCircleRight>
+              </div>
+            </div>
+          </Link>
+        </div>
+>>>>>>> Stashed changes
       </div>
     </div>
   );
